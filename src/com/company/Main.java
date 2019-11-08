cb60e39c2f9180233ba1b1d0e2f3701302fc3b9a
Бондарь Николай <Bufod>
2019-11-08 19:40:16 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr1 = new int[n][n], arr2 = new int[n][n];
+        for (int i = 0; i < arr1.length; i++)
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] = 1 + (int) (Math.random() * 10);
+                arr2[i][j] = 1 + (int) (Math.random() * 10);
+            }
+
+        // Вывод 1 матрицы
+        for (int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr1[i].length; j++){
+                System.out.print("\t" + arr1[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //Вывод 2 матрицы
+        for (int i = 0; i < arr2.length; i++){
+            for (int j = 0; j < arr2[i].length; j++){
+                System.out.print("\t" +arr2[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // результат (сложение)
+        for (int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr1[i].length; j++){
+                System.out.print("\t" + (arr2[i][j] + arr1[i][j]));
+            }
+            System.out.println();
+        }
+    }
+}
+
